725ae498befbd0971f3fd4316889258f754afa6b
Niklas <Niklas@Nikis-MBP-2.fritz.box>
2018-10-29 22:49:25 +0100

    shellsim ist fertig!

--- Blatt02/src/shellsim.c
+++ Blatt02/src/shellsim.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-    
-    char buf[256] = {0};
-    char exitString[5] = "exit";
 
+// fertig, bitte nichts ändern
+int main(int argc, char **argv) {
+    char buf[128] = {0};
+    char exitString[6] = "exit\n";
     while (1)
     {
-        //int counter = read(0, buf, sizeof(buf)) - 1;
-        fgets(buf, sizeof(buf), buf);
-
-        if (!strncmp(buf, exitString, 4))
+        fgets(buf, 128, stdin);
+        if (!strncmp(buf, exitString, 5))
         {
             return 0;
         }
         else
         {
-            int systemCommand = system(buf);
+            system(buf);
         }
     }
 }
