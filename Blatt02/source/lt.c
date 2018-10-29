6c43f674da071a82408d2a303997a7b3874fae5d
Aaron Heines <aaron.heines@tum.de>
2018-10-29 14:52:12 +0100

    include fail

--- Blatt02/source/lt.c
+++ Blatt02/source/lt.c
@@ -1,8 +1,4 @@
+// #include funktioniert bei mir nicht
 // hier soll die Main Funktion geschrieben werden
-
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
 int main(int argc, char **argv) {
 }
