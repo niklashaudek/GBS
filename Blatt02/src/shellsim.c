2511aec978995a72ddb608b10ccbb25e8d36f541
Niklas <Niklas@w205-1m-v4.eduroam.dynamic.rbg.tum.de>
2018-10-29 15:37:22 +0100

    compiled shellsim for the first time

--- Blatt02/src/shellsim.c
+++ Blatt02/src/shellsim.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-    
     while (1)
     {
         char buf[256] = {0};
         int counter = read(0, buf, sizeof(buf)) - 1;
 
-    }
-    
 
-    return 0;
+        if (strncmp(buf, "exit", 4))
+        {
+            return 0;
+        }
+    }
 }
