544ab456e9e1926ca4dde52f9f59ac6342c22d4d
Niklas <Niklas@w205-1m-v4.eduroam.dynamic.rbg.tum.de>
2018-10-29 15:48:50 +0100

    strncmp in shellsim.c added

--- Blatt02/src/shellsim.c
+++ Blatt02/src/shellsim.c
@@ -3,13 +3,15 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
+    
+    char buf[256] = {0};
+    char exitString[5] = "exit";
+
     while (1)
     {
-        char buf[256] = {0};
         int counter = read(0, buf, sizeof(buf)) - 1;
 
-
-        if (strncmp(buf, "exit", 4))
+        if (!strncmp(buf, exitString, 4))
         {
             return 0;
         }
