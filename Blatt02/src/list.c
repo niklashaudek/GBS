4272f276e5161e52e0f320d4013b6d3736c072e8
Aaron Heines <aaron.heines@tum.de>
2018-10-29 18:15:17 +0100

    Funktionsdeklarationen eingefügt

--- Blatt02/src/list.c
+++ Blatt02/src/list.c
@@ -1,2 +1,31 @@
+#include <list.h>
 // hier sollen die Funktionen implementiert werden
+
+list_t *list_init (){
+
+}
+
+struct list_elem *list_insert (list_t *list, char *data){
+
+}
+
+struct list_elem *list_append (list_t *list, char *data){
+
+}
+
+int list_remove (list_t *list, struct list_elem *elem){
+
+}
+
+void list_finit (list_t *list){
+
+}
+
+struct list_elem *list_find (list_t *list,char *data, int (*cmp_elem) (const char *, const char*)){
+
+}
+
+void list_print (list_t *list, char (*print_elem) (char *)){
+
+}
 
