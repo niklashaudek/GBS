74b1564af57a10ba37a3f8bf9549f98577242d71
Niklas <Niklas@Nikis-MacBook-Pro-2.local>
2018-10-31 10:18:44 +0100

    tests

--- Blatt02/src/list.c
+++ Blatt02/src/list.c
@@ -4,74 +4,86 @@
 #include <string.h>
 // hier sollen die Funktionen implementiert werden
 
-list_t *list_init (){ // noch unsicher
-list_t = *list;
-*list->first = NULL;
-*list->last = NULL;
-return *list;
+list_t *list_init () // noch unsicher
+{
+    return (list_t*) malloc (sizeof(list_t*));
+    /*
+    list_t = *list;
+    *list->first = NULL;
+    *list->last = NULL;
+    return *list;
+    */
 }
 
-struct list_elem *list_insert (list_t *list, char *data){ 
-list_elem element = malloc(sizeof(list_elem));
-*element->data = *data; // darf element jeweils ein Pointer sein?
-*element->next = *list->first;
-*list->first = *element; // darf element hier ein Pointer sein?
+struct list_elem *list_insert (list_t *list, char *data)
+{
+    list_elem element = malloc(sizeof(list_elem));
+    
+    *element->data = *data; // darf element jeweils ein Pointer sein?
+    *element->next = *list->first;
+    *list->first = *element; // darf element hier ein Pointer sein?
 }
 
-struct list_elem *list_append (list_t *list, char *data){
-list_elem element = malloc(sizeof(list_elem));
-*element->data = *data; // darf element jeweils ein Pointer sein?
-*list->last = *element;
-*element->next = NULL;
+struct list_elem *list_append (list_t *list, char *data)
+{
+    list_elem element = malloc(sizeof(list_elem));
+    *element->data = *data; // darf element jeweils ein Pointer sein?
+    *list->last = *element;
+    *element->next = NULL;
 }
 
-int list_remove (list_t *list, struct list_elem *elem){
-list_elem *comp = *list->first; // Anfang der Liste ermitteln
-char *data_comp_1 = *comp->data; // Daten des Anfangs ermitteln
-char *data_comp_2 = *elem->data; // Daten des gesuchten Elementes ermitteln
+int list_remove (list_t *list, struct list_elem *elem)
+{
+    list_elem *comp = *list->first; // Anfang der Liste ermitteln
+    char *data_comp_1 = *comp->data; // Daten des Anfangs ermitteln
+    char *data_comp_2 = *elem->data; // Daten des gesuchten Elementes ermitteln
 
-while(strcmp(*list->last, data_comp_1) == 0){ 
-    if(strcmp(*data_comp_1, *data_comp_2)){
-        //remove object
-        return 0;
-    }
-    *comp = *comp->next; // auf nächstes Objekt der Liste springen
-    *data_comp_1 = *comp->data; // erneut Daten zwischenspeichern
-} 
+    while(strcmp(*list->last, data_comp_1) == 0){ 
+        if(strcmp(*data_comp_1, *data_comp_2)){
+            //remove object
+            return 0;
+        }
+        *comp = *comp->next; // auf nächstes Objekt der Liste springen
+        *data_comp_1 = *comp->data; // erneut Daten zwischenspeichern
+    } 
+    return -1; // Objekt wurde nicht gefunden
+}
+
+void list_finit (list_t *list)
+{
+    free(*list);
+    *list->first = NULL;
+    *list->last = NULL;
+}
+
+struct list_elem *list_find (list_t *list,char *data, int (*cmp_elem) (const char *, const char*))
+{
+    list_elem *comp = *list->first; // Anfang der Liste ermitteln
+    char *data_comp_1 = *comp->data; // Daten des Anfangs ermitteln
+    char *data_comp_2 = *elem->data; // Daten des gesuchten Elementes ermitteln
+
+    while(strncmp(*list->last, data_comp_1, ) == 0){ 
+        if((*data_comp_1, *data_comp_2)){
+            //remove object
+            return 0;
+        }
+        *comp = *comp->next; // auf nächstes Objekt der Liste springen
+        *data_comp_1 = *comp->data; // erneut Daten zwischenspeichern
+    } 
 return -1; // Objekt wurde nicht gefunden
 }
 
-void list_finit (list_t *list){
-free(*list);
-*list->first = NULL;
-*list->last = NULL;
-}
-
-struct list_elem *list_find (list_t *list,char *data, int (*cmp_elem) (const char *, const char*)){
-list_elem *comp = *list->first; // Anfang der Liste ermitteln
-char *data_comp_1 = *comp->data; // Daten des Anfangs ermitteln
-char *data_comp_2 = *elem->data; // Daten des gesuchten Elementes ermitteln
-
-while(strncmp(*list->last, data_comp_1, ) == 0){ 
-    if((*data_comp_1, *data_comp_2)){
-        //remove object
-        return 0;
-    }
-    *comp = *comp->next; // auf nächstes Objekt der Liste springen
-    *data_comp_1 = *comp->data; // erneut Daten zwischenspeichern
-} 
-return -1; // Objekt wurde nicht gefunden
-}
-
-void list_print (list_t *list, char (*print_elem) (char *)){
+void list_print (list_t *list, char (*print_elem) (char *))
+{
 
 }
 
-void print_string (char *data){
+void print_string (char *data)
+{
 
 }
 
-int (*cmp_elem) (const char *, const char*){
+int (*cmp_elem) (const char *, const char*)
+{
 
 }
-
--- Blatt02/src/lt.c
+++ Blatt02/src/lt.c
@@ -9,13 +9,17 @@
 
 // diese Main ist ein Test vom Sheet
 int main (int argc, char *argv[] , char *envp[] ) {
-list_t *li;
-i f (( li = list_init()) == NULL) {
-perror ("Cannot allocate memory " ) ; exit(1);
-} 
-if (list_append (li , "erstes") == NULL || list_append (li, "zweites" ) == NULL || list_append (li, "drittes") == NULL ) {
-perror("Cannot allocate memory") ; 
-exit(1);} 
-list_print(li,print_string); 
-exit(0);
+    list_t *li;
+    if (( li = list_init()) == NULL)
+    {
+        perror ("Cannot allocate memory " );
+        exit(1);
+    } 
+    if (list_append (li , "erstes") == NULL || list_append (li, "zweites" ) == NULL || list_append (li, "drittes") == NULL )
+    {
+        perror ("Cannot allocate memory"); 
+        exit(1);
+    } 
+    list_print(li,print_string); 
+    exit(0);
 }
