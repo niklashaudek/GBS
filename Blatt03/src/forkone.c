6e52c6da761fcf71456c25beb66505eb1cda4fb7
Aaron Heines <aaron.heines@tum.de>
2018-11-06 22:45:47 +0100

    Thread Example from Lecture

--- Blatt03/src/forkone.c
+++ Blatt03/src/forkone.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    printf("Hello, world\n");
+static void* thread_func(void *arg) {
+    char *s = (char *) arg;
+    printf("Thread TID=%d says %s\n", pthread_self, s); 
+    pthread_exit((void *)strlen(s));
+    } 
 
-    return 0;
+
+int main(int argc, char 8 9
 }
+
+char 
+argv[]) {
+pthread_t thread; void int ret = pthread_create(&thread, NULL, thread_func,
+*
+res;
+if (ret != 0)
+fprintf(stderr, "Error: Cannot start thread\n"); Wait for thread
+*
+/
+/
+* ret = pthread_join(thread, &res); if (ret != 0)
+fprintf(stderr, "Error: Cannot join thread\n");
+printf("Thread returned %ld\n",(long)res);
+exit(EXIT_SUCCESS);
+
 
