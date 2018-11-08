d50f218cd832f966b87e7b57f9e5d7e0ea1c391b
Aaron Heines <aaron.heines@tum.de>
2018-11-08 16:12:27 +0100

    Process ID Time

--- Blatt03/src/forkone.c
+++ Blatt03/src/forkone.c
@@ -2,13 +2,19 @@
 #include <string.h>
 #include <pthread.h>
 #include <time.h>
-#define EXIT_SUCCESS 0
+#include <unistd.h>
+#include <stdlib.h>
 
-static void* thread_func(void *arg) {
+static void* set_time(void *arg) {
     
     time_t now;
 	now = time(0);
-	printf("%s\n", ctime(&now)); // unsicher aus dem Internet
+	printf("Start: %s", ctime(&now)); // fertig, Datum funktioniert, nichts ändern
+    
+    //while schleife und sleep mit jeweils einer Sekunde Abstand fehlen noch
+    
+    pid_t pid = getpid(); // process id
+    printf("%d %d\n", pid, pid -1);
     void *t;
     return t;
     } 
@@ -17,11 +23,13 @@ static void* thread_func(void *arg) {
 int main(int argc, char *argv[]) {
     pthread_t thread;
     void *res; 
-    int ret = pthread_create(&thread, NULL, thread_func, "hello world");
+
+    int ret = pthread_create(&thread, NULL, set_time, "hello world");
 
     if (ret != 0) fprintf(stderr, "Error: Cannot start thread\n"); 
 //Wait for thread
     ret = pthread_join(thread, &res); 
+    
     if (ret != 0) fprintf(stderr, "Error: Cannot join thread\n");
 
     printf("Thread returned %ld\n",(long)res);
