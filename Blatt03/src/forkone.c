4d3d52f8c969ef7a519c592ced9ab6b118d55f5a
Aaron Heines <aaron.heines@tum.de>
2018-11-08 12:55:24 +0100

    .

--- Blatt03/src/forkone.c
+++ Blatt03/src/forkone.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <pthread.h>
 #define EXIT_SUCCESS 0
 
-// static void* thread_func(void *arg) {
+static void* thread_func(void *arg) {
     
-//     time_t now; 
-// 	now = time(0);
-// 	printf("%s\n", ctime(&now)); // unsicher aus dem Internet
-//     int count = scanf("%i", &count);
-//     char *s = (char *) arg;
-//     printf("Thread TID=%d says %s\n", pthread_self, s); 
-//     pthread_exit((void *)strlen(s));
-//     } 
+    time_t now; 
+	now = time(0);
+	printf("%s\n", ctime(&now)); // unsicher aus dem Internet
+    } 
 
 
-// int main(int argc, char argv[]) {
-//     pthread_t thread; 
-//     void *res; 
-//     int ret = pthread_create(&thread, NULL, thread_func, "hello world");
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    void *res; 
+    int ret = pthread_create(&thread, NULL, thread_func, "hello world");
 
-// if (ret != 0) fprintf(stderr, "Error: Cannot start thread\n"); 
-// //Wait for thread
-// ret = pthread_join(thread, &res); 
-// if (ret != 0) fprintf(stderr, "Error: Cannot join thread\n");
+    if (ret != 0) fprintf(stderr, "Error: Cannot start thread\n"); 
+//Wait for thread
+    ret = pthread_join(thread, &res); 
+    if (ret != 0) fprintf(stderr, "Error: Cannot join thread\n");
 
-// printf("Thread returned %ld\n",(long)res);
-
-// exit(EXIT_SUCCESS);
-// }
+    printf("Thread returned %ld\n",(long)res);
+    //Threads beenden sich mit Ende der main automatisch
+}
 
