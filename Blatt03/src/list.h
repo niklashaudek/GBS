fdee6c78ffbe94741070d934de4458ea156a2058
Niklas <Niklas@Nikis-MBP-2.fritz.box>
2018-11-11 17:58:26 +0100

    forkmany

--- Blatt03/src/list.h
+++ Blatt03/src/list.h
@@ -23,7 +23,3 @@ void             list_finit (list_t *list);
 void             list_print (list_t *list, void (*print_elem) (char *));
 struct list_elem *list_find (list_t *list, char *data, int (*cmp_elem) (const char *, const char *));
 void             print_string (char *data); // Muss die Deklaration auch hier stehen?
-
-
-
-
