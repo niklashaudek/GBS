d10e05e510bca10f5d34a2cbda4f82d4a618b8d9
Niklas <Niklas@Nikis-MacBook-Pro-2.fritz.box>
2018-11-11 12:32:38 +0100

    forkone fertig

--- Blatt03/src/forkone.c
+++ Blatt03/src/forkone.c
@@ -3,34 +3,44 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-// static void* set_time(void *arg) {
-    
-//     time_t now;
-// 	    now = time(0);
-// 	    printf("Start: %s", ctime(&now)); // fertig, Datum funktioniert, nichts ändern
-//     //while schleife und sleep mit jeweils einer Sekunde Abstand fehlen noch
-//     pid_t pid = getpid(); // process id
-//     printf("%d %d\n", pid, pid -1);
-//     void *t;
-//     return t;
-//     } 
-
+#include <errno.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
     
+    if (argc < 2)
+    {
+        perror ("Missing parameter K.");
+        exit (-1);
+    }
+
+/*
+    for (int idx = 0; idx < strlen(argv[1]); idx++)
+    {
+        if (isdigit(argv[1][idx]) == 0)
+        {
+            perror ("Parameter K is no integer.");
+            exit (-2);
+        }
+    }
+*/
+    for( char* pChr = argv[1]; *pChr != 0; pChr++ )
+    {
+        if (isdigit(*pChr) == 0)
+        {
+            perror ("Parameter K is no integer.");
+            exit (-2);
+        }
+    }
+
     time_t now;
 	now = time(0);
 	printf("Start: %s", ctime(&now));
     
+    int k = atoi(argv[1]);
+
     int newProcessPid = fork();
-
-    // hier muss Eingabe auf Integer geprüft werden
-    // if ()
-    // int k = (int*) argv[1];
     
-    int k = 10;
-
     int exitCode = (int) (newProcessPid+k)%100; // hier wird schon der Exit-Code berechnet
 
     switch(newProcessPid) {
@@ -46,8 +56,9 @@ int main(int argc, char *argv[]) {
                 }
                 break;
             default:
+                // printf("vor wait\n");
                 waitpid(newProcessPid, NULL, 0); // Make sure we wait on the child process to prevent it from getting a Zombie process
-                printf("Exit-Code: %d\n", exitCode);
+                printf("Exit-Code: %d\n", exitCode); 
                 now = time(0);
 	            printf("Ende: %s", ctime(&now));
                 break;
