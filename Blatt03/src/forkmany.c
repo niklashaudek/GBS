dba9abbf565b709ba3fe65d9a79ec6bfc128e943
Niklas <Niklas@Nikis-MBP-2.fritz.box>
2018-11-11 17:44:39 +0100

    forkmany

---
+++ Blatt03/src/forkmany.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+
+    int c;
+    int iKparam = 10;
+    int iNparam = 1;
+    int iRparam = 0;
+
+    opterr = 0;
+
+    while ((c = getopt (argc, argv, "-k:-n:-r")) != -1)
+    {
+        switch (c)
+        {
+        case 'k':
+            for( char* pChr = optarg; *pChr != 0; pChr++ )
+            {
+                if (isdigit(*pChr) == 0)
+                {
+                    perror ("Parameter K is no integer.");
+                    exit (-2);
+                }
+            }
+            iKparam = atoi(optarg);
+            break;
+        case 'n':
+            for( char* pChr = optarg; *pChr != 0; pChr++ )
+            {
+                if (isdigit(*pChr) == 0)
+                {
+                    perror ("Parameter N is no integer.");
+                    exit (-3);
+                }
+            }
+            iNparam = atoi(optarg);
+            break;
+        case 'r':
+            iRparam = 1;
+            break;
+        default:
+            abort ();
+        }
+    }
+
+    time_t now;
+	now = time(0);
+	printf("Start: %s", ctime(&now));
+
+    int newProcessPid = fork();
+    
+    int exitCode = (int) (newProcessPid+iKparam)%100; // hier wird schon der Exit-Code berechnet
+
+    switch(newProcessPid) {
+            case -1:
+                perror ("fork () failed");
+                break;
+
+            case 0:
+                for (int i = 1; i <= iKparam; i++)
+                {
+                    sleep(1);
+                    printf("%d %d %d\n", getpid(), getppid(), i);
+                }
+                break;
+            default:
+                // printf("vor wait\n");
+                waitpid(newProcessPid, NULL, 0); // Make sure we wait on the child process to prevent it from getting a Zombie process
+                printf("Exit-Code: %d\n", exitCode); 
+                now = time(0);
+	            printf("Ende: %s", ctime(&now));
+                break;
+        }
+}
---
+++ Blatt03/src/list.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Hier soll die Datenstruktur und die Funktionsprototypen definiert werden
+
+struct list_elem {
+    struct list_elem    *next;  // Zeiger auf das naechste Element
+    char	            *data;  // Zeiger auf ein Datenobject
+};
+
+typedef struct list {
+	struct list_elem *first;    // erstes Element in der Liste
+	struct list_elem *last;     // letztes Element in der Liste
+} list_t;
+
+/* function prototypes */
+list_t           *list_init ();
+struct list_elem *list_insert (list_t *list, char *data);
+struct list_elem *list_append (list_t *list, char *data);
+int              list_remove (list_t *list, struct list_elem *elem);
+void             list_finit (list_t *list);
+void             list_print (list_t *list, void (*print_elem) (char *));
+struct list_elem *list_find (list_t *list, char *data, int (*cmp_elem) (const char *, const char *));
+void             print_string (char *data); // Muss die Deklaration auch hier stehen?
+
+
+
+
