92bf6ffb8be7b6d8a810b7ddabb481cccdc12ad9
Aaron Heines <aaron.heines@tum.de>
2018-11-14 21:42:06 +0100

    liste fertig

--- Blatt04/src/list.c
+++ Blatt04/src/list.c
@@ -30,5 +30,22 @@ struct list_elem *list_append (list_t *list, pthread_t data) // fertig
 
 struct list_elem *list_append_thread (list_t *list, int iThreadNumber, int iThreadPrio, int iThreadStarttime, int iThreadLaufzeit)
 {
-    
+    struct list_elem* pElement = (struct list_elem*) malloc(sizeof(struct list_elem));
+    pElement->iThreadNumber = iThreadNumber;
+    pElement->iThreadPrio = iThreadPrio;
+    pElement->iThreadStarttime = iThreadStarttime;
+    pElement->iThreadLaufzeit = iThreadLaufzeit;
+    if (list->last != NULL)
+    {
+        list->last->next = pElement;
+        list->last = pElement;
+        list->last->next = NULL;
+    }
+    else
+    {
+        list->last = pElement;
+        list->first = pElement;
+        list->first->next = NULL;
+    }
+    return pElement;
 }
