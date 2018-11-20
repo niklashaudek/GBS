d9062516df436ccd8209b09181377b2d4c2d09e9
Niki's MacBook Pro <niklas@w217-5k-v4.eduroam.dynamic.rbg.tum.de>
2018-11-20 10:32:55 +0100

    test

--- Blatt05/src/minithread.c
+++ Blatt05/src/minithread.c
@@ -1,3 +1,15 @@
+#define _GNU_SOURCE
+
 #include <stdio.h> // For printf(...) and sprintf(...)
 #include <pthread.h> // For thread stuff
 #include <stdlib.h> // For malloc(...)
+
+void thread_func (char* string) {
+    printf("Hello, my name is %s!\n", string);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    const char* names[5] = { "up", "you", "give", "gonna", "never" };
+
+}
